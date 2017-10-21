66a5cc863278409dfeab652c3a4da05ad622afef
Prof. 9 <prof9@outlook.com>
2017-10-21 04:14:31 +0200

    Add Generate-Tilemap command.

---
+++ PixelPet/Commands/GenerateTilemapCmd.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PixelPet.Commands {
+	internal class GenerateTilemapCmd : CliCommand {
+		protected struct TilemapEntry {
+			int TileNum { get; set; }
+			int Palette { get; set; }
+			bool HFlip { get; set; }
+			bool VFlip { get; set; }
+		}
+
+		public GenerateTilemapCmd()
+			: base("Generate-Tilemap") { }
+
+		public override void Run(Workbench workbench, Cli cli) {
+			Tilemap tm = new Tilemap(workbench.Bitmap, 8, 8);
+			tm.Reduce(true);
+
+			workbench.Stream.SetLength(0);
+
+			foreach (Tilemap.Entry entry in tm.TileEntries) {
+				int scrn = 0;
+				scrn |= entry.TileNumber & 0x1FF;
+				scrn |= entry.FlipHorizontal ? 1 << 10 : 0;
+				scrn |= entry.FlipVertical   ? 1 << 11 : 0;
+				workbench.Stream.WriteByte((byte)(scrn >> 0));
+				workbench.Stream.WriteByte((byte)(scrn >> 8));
+			}
+		}
+	}
+}
