346cfeda2278d380521a4a771e0166720d6ecbeb
Prof. 9 <prof9@outlook.com>
2018-02-08 18:58:21 +0100

    Add --base-tile option to Deserialize-Tilemap.

--- PixelPet/Commands/DeserializeTilemapCmd.cs
+++ PixelPet/Commands/DeserializeTilemapCmd.cs
@@ -6,9 +6,13 @@ using System.Text;
 namespace PixelPet.Commands {
 	internal class DeserializeTilemapCmd : CliCommand {
 		public DeserializeTilemapCmd()
-			: base("Deserialize-Tilemap") { }
+			: base("Deserialize-Tilemap", new Parameter[] {
+				new Parameter("base-tile", "bt", false, new ParameterValue("index", "0"))
+			}) { }
 
 		public override void Run(Workbench workbench, Cli cli) {
+			int baseTile = FindNamedParameter("--base-tile").Values[0].ToInt32();
+
 			cli.Log("Deserializing tilemap...");
 
 			workbench.Stream.Position = 0;
@@ -21,7 +25,7 @@ namespace PixelPet.Commands {
 				int scrn = buffer[0] | (buffer[1] << 8);
 
 				TileEntry te = new TileEntry() {
-					TileNumber = scrn & 0x3FF,
+					TileNumber = (scrn & 0x3FF) - baseTile,
 					HFlip = (scrn & (1 << 10)) != 0,
 					VFlip = (scrn & (1 << 11)) != 0,
 					PaletteNumber = (scrn >> 12) & 0xF
