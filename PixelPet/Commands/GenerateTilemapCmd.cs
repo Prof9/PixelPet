32a150bc6af70cc3268a06197d86a912f5c41405
Prof. 9 <prof9@outlook.com>
2018-02-08 17:43:16 +0100

    Change some parameter shorthands.

--- PixelPet/Commands/GenerateTilemapCmd.cs
+++ PixelPet/Commands/GenerateTilemapCmd.cs
@@ -10,14 +10,14 @@ namespace PixelPet.Commands {
 	internal class GenerateTilemapCmd : CliCommand {
 		public GenerateTilemapCmd()
 			: base("Generate-Tilemap", new Parameter[] {
-				new Parameter("palette-size", "p", false, new ParameterValue("count",  "16")),
-				new Parameter("no-reduce",    "r", false),
+				new Parameter("palette-size", "ps", false, new ParameterValue("count",  "16")),
+				new Parameter("no-reduce",    "nr", false),
 			}) { }
 
 		public override void Run(Workbench workbench, Cli cli) {
 			cli.Log("Generating tilemap...");
 
-			int palSize = FindNamedParameter("--palette-size").Values[0].ToInt32();
+			int palSize   = FindNamedParameter("--palette-size").Values[0].ToInt32();
 			bool noReduce = FindNamedParameter("--no-reduce").IsPresent;
 
 			workbench.Tilemap = new Tilemap(workbench.Bitmap, workbench.Tileset, workbench.Palette, palSize, !noReduce);
