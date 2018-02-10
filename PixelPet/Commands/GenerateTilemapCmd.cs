bdb52a2b25af651a831249d087907f8ffb5640f1
Prof. 9 <prof9@outlook.com>
2018-02-11 00:55:38 +0100

    Add cropping options into Generate-Tilemap directly.

--- PixelPet/Commands/GenerateTilemapCmd.cs
+++ PixelPet/Commands/GenerateTilemapCmd.cs
@@ -9,18 +9,28 @@ using System.Text;
 namespace PixelPet.Commands {
 	internal class GenerateTilemapCmd : CliCommand {
 		public GenerateTilemapCmd()
-			: base("Generate-Tilemap", new Parameter[] {
+			: base("Generate-Tilemap",
 				new Parameter("palette-size", "ps", false, new ParameterValue("count",  "16")),
 				new Parameter("no-reduce",    "nr", false),
-			}) { }
+				new Parameter("x",            "x",  false, new ParameterValue("pixels", "0")),
+				new Parameter("y",            "y",  false, new ParameterValue("pixels", "0")),
+				new Parameter("width",        "w",  false, new ParameterValue("pixels", "-1")),
+				new Parameter("height",       "h",  false, new ParameterValue("pixels", "-1"))
+			) { }
 
 		public override void Run(Workbench workbench, Cli cli) {
 			cli.Log("Generating tilemap...");
 
 			int  palSize  = FindNamedParameter("--palette-size").Values[0].ToInt32();
-			bool noReduce = FindNamedParameter("--no-reduce").IsPresent;
+			bool noReduce = FindNamedParameter("--no-reduce"   ).IsPresent;
+			int  x        = FindNamedParameter("--x"           ).Values[0].ToInt32();
+			int  y        = FindNamedParameter("--y"           ).Values[0].ToInt32();
+			int  w        = FindNamedParameter("--width"       ).Values[0].ToInt32();
+			int  h        = FindNamedParameter("--height"      ).Values[0].ToInt32();
 
-			workbench.Tilemap = new Tilemap(workbench.Bitmap, workbench.Tileset, workbench.Palette, palSize, !noReduce);
+			using (Bitmap bmp = workbench.GetCroppedBitmap(x, y, w, h, cli)) {
+				workbench.Tilemap = new Tilemap(bmp, workbench.Tileset, workbench.Palette, palSize, !noReduce);
+			}
 		}
 	}
 }
