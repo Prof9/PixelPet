b26631d138dff85b8f3b7dc8ecb9bd1c6c36e16d
Prof. 9 <prof9@outlook.com>
2018-04-19 00:53:44 +0200

    Render-Tileset: Add --tiles-per-row parameter.

--- PixelPet/Commands/RenderTilesetCmd.cs
+++ PixelPet/Commands/RenderTilesetCmd.cs
@@ -6,10 +6,12 @@ using System.Text;
 namespace PixelPet.Commands {
 	internal class RenderTilesetCmd : CliCommand {
 		public RenderTilesetCmd()
-			: base("Render-Tileset") { }
+			: base("Render-Tileset",
+				new Parameter("tiles-per-row", "tw", false, new ParameterValue("count", "32"))
+			) { }
 
 		public override void Run(Workbench workbench, Cli cli) {
-			int maxTilesPerRow = 0;
+			int maxTilesPerRow = FindNamedParameter("--tiles-per-row").Values[0].ToInt32();
 
 			workbench.SetBitmap(workbench.Tileset.ToBitmap(maxTilesPerRow));
 		}
