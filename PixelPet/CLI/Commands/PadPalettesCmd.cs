288a6b9b8d301bded0a14f4238a9bd82f47f044f
Prof. 9 <prof9@outlook.com>
2018-09-27 14:35:10 +0200

    Pad-Palettes: Add parameter specifying color to pad with.

--- PixelPet/CLI/Commands/PadPalettesCmd.cs
+++ PixelPet/CLI/Commands/PadPalettesCmd.cs
@@ -5,11 +5,13 @@ namespace PixelPet.CLI.Commands {
 	internal class PadPalettesCmd : CliCommand {
 		public PadPalettesCmd()
 			: base("Pad-Palettes",
-				new Parameter(true, new ParameterValue("width", "0"))
+				new Parameter(true, new ParameterValue("width", "0")),
+				new Parameter("color", "c", false, new ParameterValue("value", "0"))
 			) { }
 
 		public override void Run(Workbench workbench, ILogger logger) {
 			int width = FindUnnamedParameter(0).Values[0].ToInt32();
+			int color = FindNamedParameter("--color").Values[0].ToInt32();
 
 			if (width < 1) {
 				logger?.Log("Invalid palette width.", LogLevel.Error);
@@ -25,7 +27,7 @@ namespace PixelPet.CLI.Commands {
 			int addedColors = 0;
 			foreach (PaletteEntry pe in workbench.PaletteSet) {
 				while (pe.Palette.Count < width) {
-					pe.Palette.Add(0);
+					pe.Palette.Add(color);
 					addedColors++;
 				}
 			}
