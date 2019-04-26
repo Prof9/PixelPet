e0c87d71dcf74b3e140576e139134ee156cc3c85
Prof. 9 <prof9@outlook.com>
2019-04-26 15:45:11 +0200

    Allow tile size to be changed from 8x8 using Clear-Tileset --tile-size.

--- PixelPet/CLI/Commands/ClearTilesetCmd.cs
+++ PixelPet/CLI/Commands/ClearTilesetCmd.cs
@@ -4,15 +4,17 @@ using System;
 namespace PixelPet.CLI.Commands {
 	internal class ClearTilesetCmd : CliCommand {
 		public ClearTilesetCmd()
-			: base("Clear-Tileset") { }
+			: base("Clear-Tileset",
+				new Parameter("tile-size", "s", false, new ParameterValue("width", "8"), new ParameterValue("height", "8"))
+			) { }
 
 		public override void Run(Workbench workbench, ILogger logger) {
-			int w = 8;
-			int h = 8;
+			int tw = FindNamedParameter("--tile-size").Values[0].ToInt32();
+			int th = FindNamedParameter("--tile-size").Values[1].ToInt32();
 
-			workbench.Tileset = new Tileset(w, h);
+			workbench.Tileset = new Tileset(tw, th);
 
-			logger?.Log("Created new " + w + 'x' + h + " tileset.");
+			logger?.Log("Created new " + tw + 'x' + th + " tileset.");
 		}
 	}
 }
