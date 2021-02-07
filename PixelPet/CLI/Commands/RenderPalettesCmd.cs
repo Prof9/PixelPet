cb6fe7614538bda97598904dea74ec3154e1bd0d
Prof. 9 <prof9@outlook.com>
2021-02-07 22:38:46 +0100

    Render-Palettes: Add --colors-per-row argument for e.g. 16x16 rendering of 256-color palette.

--- PixelPet/CLI/Commands/RenderPalettesCmd.cs
+++ PixelPet/CLI/Commands/RenderPalettesCmd.cs
@@ -7,14 +7,29 @@ using System.Linq;
 namespace PixelPet.CLI.Commands {
 	internal class RenderPalettesCmd : CliCommand {
 		public RenderPalettesCmd()
-			: base("Render-Palettes") { }
+			: base("Render-Palettes",
+				new Parameter("colors-per-row", "cw", false, new ParameterValue("count", "0"))
+			) { }
 
 		protected override void Run(Workbench workbench, ILogger logger) {
+			int maxTilesPerRow = FindNamedParameter("--colors-per-row").Values[0].ToInt32();
+			if (maxTilesPerRow < 0) {
+				logger?.Log("Invalid colors per row.", LogLevel.Error);
+				return;
+			}
+
 			int tw = 8;
 			int th = 8;
 
-			int w = workbench.PaletteSet.Max(pe => pe.Palette.Count);
-			int h = workbench.PaletteSet.Count;
+			int maxColors = workbench.PaletteSet.Max(pe => pe.Palette.Count);
+			int palCount  = workbench.PaletteSet.Count;
+
+			if (maxTilesPerRow == 0) {
+				maxTilesPerRow = maxColors;
+			}
+
+			int w = maxTilesPerRow;
+			int h = workbench.PaletteSet.Sum(pe => (pe.Palette.Count + maxTilesPerRow - 1) / maxTilesPerRow);
 
 			if (w <= 0 || h <= 0) {
 				logger?.Log("Cannot render empty palette set.", LogLevel.Error);
@@ -28,12 +43,24 @@ namespace PixelPet.CLI.Commands {
 			workbench.ClearBitmap(w * tw, h * th);
 			workbench.Graphics.CompositingMode = CompositingMode.SourceCopy;
 			using (SolidBrush brush = new SolidBrush(Color.Black)) {
-				for (int i = 0; i < workbench.PaletteSet.Count; i++) {
-					Palette pal = workbench.PaletteSet[i].Palette;
-					for (int j = 0; j < pal.Count; j++) {
-						brush.Color = Color.FromArgb(fmt.Convert(pal[j], pal.Format));
-						workbench.Graphics.FillRectangle(brush, j * tw, i * th, tw, th);
+				int p = 0;
+				int c = 0;
+				for (int j = 0; j < h; j++) {
+					Palette pal = workbench.PaletteSet[p].Palette;
+					for (int i = 0; i < w; i++) {
+						// Draw transparent if we ran out of colors.
+						if (c >= pal.Count) {
+							brush.Color = Color.Transparent;
+						} else {
+							brush.Color = Color.FromArgb(fmt.Convert(pal[c++], pal.Format));
+						}
+						workbench.Graphics.FillRectangle(brush, i * tw, j * th, tw, th);
 						count++;
+					}
+					// Go to next palette.
+					if (c >= pal.Count) {
+						p++;
+						c = 0;
 					}
 				}
 			}
