cf8683e0e6386f70393ae3f1931f7ae66e761d03
Prof. 9 <prof9@outlook.com>
2021-10-23 16:17:29 +0200

    Pad-Palettes: Add --palette-size option to specify max size of added palette.

--- PixelPet/CLI/Commands/PadPalettesCmd.cs
+++ PixelPet/CLI/Commands/PadPalettesCmd.cs
@@ -6,21 +6,27 @@ namespace PixelPet.CLI.Commands {
 		public PadPalettesCmd()
 			: base("Pad-Palettes",
 				new Parameter(true, new ParameterValue("width", "0")),
-				new Parameter("color", "c", false, new ParameterValue("value", "0"))
+				new Parameter("color", "c", false, new ParameterValue("value", "0")),
+				new Parameter("palette-size", "ps", false, new ParameterValue("count", "-1"))
 			) { }
 
 		protected override void Run(Workbench workbench, ILogger logger) {
 			int width = FindUnnamedParameter(0).Values[0].ToInt32();
 			int color = FindNamedParameter("--color").Values[0].ToInt32();
+			int palSize = FindNamedParameter("--palette-size").Values[0].ToInt32();
 
 			if (width < 1) {
 				logger?.Log("Invalid palette width.", LogLevel.Error);
 				return;
 			}
+			if (palSize == 0 || palSize < -1) {
+				logger?.Log("Invalid palette size.", LogLevel.Error);
+				return;
+			}
 
 			if (workbench.PaletteSet.Count == 0) {
-				logger?.Log("No palettes to pad. Creating 1 palette based on current bitmap format.", LogLevel.Information);
-				Palette pal = new Palette(workbench.BitmapFormat, -1);
+				logger?.Log("No palettes to pad. Creating 1 " + (palSize == -1 ? "unbounded palette" : ("palette with max size " + palSize)) + " based on current bitmap format.", LogLevel.Information);
+				Palette pal = new Palette(workbench.BitmapFormat, palSize);
 				workbench.PaletteSet.Add(pal);
 			}
 
